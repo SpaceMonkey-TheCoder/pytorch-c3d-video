9d673ea85ecf71d5b1bb530e0dc3176ced198696
SpaceMonkey <abir45pro@gmail.com>
2022-09-22 17:03:16 -0500

    Add files via upload

--- extract_video_frames.py
+++ extract_video_frames.py
@@ -1,22 +1,20 @@
 import os, shutil
 
-BASE_DIR = os.curdir
-
 def extract_video_frames (video_path, frame_path):
-    video_path = os.path.abspath(os.path.join(BASE_DIR, video_path))
+    video_path = os.path.abspath(os.path.join(os.curdir, video_path))
     print(video_path)
     print(frame_path)
-    os.system('ffmpeg -i ' + video_path + ' -q:v 2 -f image2 ' + os.path.abspath(os.path.join(os.path.join(BASE_DIR, frame_path), '/image_%5d.jpg')))
+    os.system('ffmpeg -i ' + video_path + ' -q:v 2 -f image2 ' + os.path.abspath(os.path.join(os.path.join(os.curdir, frame_path), '/image_%5d.jpg')))
     return
 
 def list_video_dir (data_dir):
-    data_dir = os.path.abspath(os.path.join(BASE_DIR, data_dir))
+    data_dir = os.path.abspath(os.path.join(os.curdir, data_dir))
     if (not os.path.exists(data_dir)):
         print("Video director {} not found!".format(data_dir))
     return os.listdir(data_dir)
 
 def make_frame_dir (frame_dir):
-    frame_dir = os.path.join(BASE_DIR, frame_dir)
+    frame_dir = os.path.join(os.curdir, frame_dir)
     if not os.path.exists(frame_dir):
         return os.mkdir(frame_dir)
     shutil.rmtree(frame_dir)
